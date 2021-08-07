14609cd29d6a316ad532b2a77480c185be08f5a7
Arco Dielhof <arcodielhof@hotmail.com>
2021-08-07 08:48:54 +0200

    Added Venthyr Pepe.

--- dataTable.lua
+++ dataTable.lua
@@ -3,7 +3,7 @@ if AZP.InterfaceCompanion == nil then AZP.InterfaceCompanion = {} end
 
 AZP.InterfaceCompanion.PepeInfo =
 {
-    Active = {1, 13, 14, 16},
+    Active = {1, 13, 14, 15, 16},
     [1] =
     {
         Name = "Standard",
@@ -110,7 +110,7 @@ AZP.InterfaceCompanion.PepeInfo =
     {
         Name = "Venthyr",
         PathName = "9xp_covenant_orangebird03.m2",
-        ModelID = 0,
+        ModelID = 3866274,
         Scale = 1,
 
     },
