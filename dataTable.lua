f246dfae773147ce3b4a3f5c650cb7727fdfef5b
Arco Dielhof <arcodielhof@hotmail.com>
2021-08-19 11:43:16 +0200

    Added Pirate Pepe.

--- dataTable.lua
+++ dataTable.lua
@@ -3,7 +3,7 @@ if AZP.InterfaceCompanion == nil then AZP.InterfaceCompanion = {} end
 
 AZP.InterfaceCompanion.PepeInfo =
 {
-    Active = {1, 4, 8, 13, 14, 15, 16},
+    Active = {1, 3, 4, 8, 13, 14, 15, 16},
     [1] =
     {
         Name = "Standard",
@@ -22,7 +22,7 @@ AZP.InterfaceCompanion.PepeInfo =
     {
         Name = "Pirate",
         PathName = "6hu_garrison_orangebird_var2.m2",
-        ModelID = 0,
+        ModelID = 1131795,
         Scale = 1,
     },
     [4] =
