18883ee6d2e4e055ef7ae07a018f0594f8e26c43
Arco Dielhof <arcodielhof@hotmail.com>
2022-03-01 11:53:04 +0100

    Update for patch 9.2 to no longer show as out of date!

--- InterfaceCompanion.lua
+++ InterfaceCompanion.lua
@@ -1,7 +1,7 @@
 if AZP == nil then AZP = {} end
 if AZP.VersionControl == nil then AZP.VersionControl = {} end
 
-AZP.VersionControl["Interface Companion"] = 25
+AZP.VersionControl["Interface Companion"] = 26
 if AZP.InterfaceCompanion == nil then AZP.InterfaceCompanion = {} end
 if AZP.InterfaceCompanion.Events == nil then AZP.InterfaceCompanion.Events = {} end
 
