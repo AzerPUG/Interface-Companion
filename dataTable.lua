c300c7bea8047fcba1f26383c118edb5c8ae0242
Arco Dielhof <arcodielhof@hotmail.com>
2022-06-03 12:33:43 +0200

    Added Elekk Plushie as Interface Companion.

--- dataTable.lua
+++ dataTable.lua
@@ -4,7 +4,7 @@ if AZP.InterfaceCompanion == nil then AZP.InterfaceCompanion = {} end
 AZP.InterfaceCompanion.PepeInfo =
 {
     StandardPath = "World\\Expansion05\\doodads\\orc\\doodads\\",
-    Active = {17, 18, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28},
+    Active = {17, 18, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29},
      [1] = {Name =     "Standard Pepe",    ModelID = 1041861,},
      [2] = {Name =       "Knight Pepe",    ModelID = 1131783,},
      [3] = {Name =       "Pirate Pepe",    ModelID = 1131795,},
@@ -33,4 +33,5 @@ AZP.InterfaceCompanion.PepeInfo =
     [26] = {Name =          "Twilight", CreatureID =  122033,},
     [27] = {Name =            "Purity", CreatureID =  171697,},
     [28] = {Name =            "Shadow", CreatureID =  123650,},
+    [29] = {Name =     "Elekk Plushie", CreatureID =   82464,},
 }
