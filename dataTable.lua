09e2bc2ea7473c66f2f597d1d7ff62a53ebc1856
Arco Dielhof <arcodielhof@hotmail.com>
2022-06-30 12:51:07 +0200

    Added Drakks as an Interface Companion.

--- dataTable.lua
+++ dataTable.lua
@@ -4,7 +4,7 @@ if AZP.InterfaceCompanion == nil then AZP.InterfaceCompanion = {} end
 AZP.InterfaceCompanion.PepeInfo =
 {
     StandardPath = "World\\Expansion05\\doodads\\orc\\doodads\\",
-    Active = {17, 18, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 31, 32},
+    Active = {17, 18, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 31, 32, 33},
      [1] = {Name =      "Standard Pepe",    ModelID = 1041861,},
      [2] = {Name =        "Knight Pepe",    ModelID = 1131783,},
      [3] = {Name =        "Pirate Pepe",    ModelID = 1131795,},
@@ -37,4 +37,5 @@ AZP.InterfaceCompanion.PepeInfo =
     [30] = {Name =      "Ghostly Skull", CreatureID =   29147,},
     [31] = {Name = "Kirin Tor Familiar", CreatureID =   32643,},
     [32] = {Name =        "Murkstrasza", CreatureID =  181535,},
+    [33] = {Name =             "Drakks", CreatureID =  181575,},
 }
