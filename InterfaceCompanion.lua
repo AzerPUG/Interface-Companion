24e0bd815f50ad5d962d90969e859f34fa233423
Arco Dielhof <arcodielhof@hotmail.com>
2022-08-18 15:44:55 +0200

    No longer showing out of date!

--- InterfaceCompanion.lua
+++ InterfaceCompanion.lua
@@ -1,7 +1,7 @@
 if AZP == nil then AZP = {} end
 if AZP.VersionControl == nil then AZP.VersionControl = {} end
 
-AZP.VersionControl["Interface Companion"] = 48
+AZP.VersionControl["Interface Companion"] = 49
 if AZP.InterfaceCompanion == nil then AZP.InterfaceCompanion = {} end
 if AZP.InterfaceCompanion.Events == nil then AZP.InterfaceCompanion.Events = {} end
 
